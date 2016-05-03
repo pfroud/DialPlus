43bb2c632de5829c921e2f11c07f775f7a3a4ac3
Peter Froud <pfroud+github@gmail.com>
2016-05-02 21:06:37 -0700

    Battery percent works

--- src/Dial.c
+++ src/Dial.c
@@ -9,209 +9,224 @@
 #define DATE_VISIBILITY_DURATION (4000)
 
 
-// Hello from CloudPebble.
-
 static Window *s_main_window;
 static BitmapLayer *s_background_layers[4];
 static GBitmap *s_background_bitmap;
-static TextLayer *s_date_layer;
+static TextLayer *s_date_layer, *s_battery_percent_layer;
 
-static GRect date_frame_onscreen;
-static GRect date_frame_offscreen;
+static GRect date_frame_onscreen, date_frame_offscreen;
 
 bool dateIsAnimating = 0;
 
 static Layer *s_battery_meter_layer;
-static TextLayer *s_battery_percent_layer;
 static int s_battery_level;
 
 // https://developer.pebble.com/tutorials/intermediate/add-battery/
-static void battery_callback(BatteryChargeState state) {
-  s_battery_level = state.charge_percent;
-  
-  layer_mark_dirty(s_battery_meter_layer);
-  layer_mark_dirty(text_layer_get_layer(s_battery_percent_layer));
+
+
+static void battery_handler(BatteryChargeState state) {
+    s_battery_level = state.charge_percent;
+    
+    static char buffer[5];
+    snprintf(buffer, sizeof(buffer), "%d%%", s_battery_level);
+    text_layer_set_text(s_battery_percent_layer, buffer);
+    
+    layer_mark_dirty(s_battery_meter_layer);
+    layer_mark_dirty(text_layer_get_layer(s_battery_percent_layer));
 
 }
 
 static void battery_meter_update_proc(Layer *layer, GContext *ctx) {
-  GRect bounds = layer_get_bounds(layer);
-  
-  int batt = s_battery_level;
-  
-  GColor theColor = GColorRed;
-  
-  if(batt >= 10) theColor = GColorOrange;
-  if(batt >= 30) theColor = GColorGreen;
-
-
-  graphics_context_set_fill_color(ctx, theColor);
-  
-
-  int barWidth = batt * bounds.size.w / 100; //can't use floating point numbers!
-  int barThickness = 3;
-  GRect theBar = GRect(0, bounds.size.h-barThickness, barWidth, barThickness); // x, y, w, h
-  graphics_fill_rect(ctx, theBar, 0, GCornerNone);
+    GRect bounds = layer_get_bounds(layer);
+    
+    int batt = s_battery_level;
+    GColor theColor = GColorRed;
+    if(batt >= 20) theColor = GColorYellow;
+    if(batt >= 40) theColor = GColorGreen;
+    graphics_context_set_fill_color(ctx, theColor);
+    
+    int barWidth = batt * bounds.size.w / 100; //can't use floating point numbers!
+    int barThickness = 3;
+    GRect theBar = GRect(0, bounds.size.h-barThickness, barWidth, barThickness); // x, y, w, h
+    graphics_fill_rect(ctx, theBar, 0, GCornerNone);
 }
-
-static void battery_percent_update_proc(Layer *layer, GContext *ctx) {
-  static char buffer[4];
-  snprintf(buffer, sizeof(buffer), "%d%%", s_battery_level);
-  text_layer_set_text(s_date_layer, buffer);
-}
-  
-
-
 
 
 static const char *days_of_week[] = {
-  "SUN",
-  "MON",
-  "TUE",
-  "WED",
-  "THU",
-  "FRI",
-  "SAT",
+    "SUN",
+    "MON",
+    "TUE",
+    "WED",
+    "THU",
+    "FRI",
+    "SAT",
 };
 
 static void date_animation_stopped_handler(Animation *animation, bool finished, void *context) {
-  dateIsAnimating = 0;
+    dateIsAnimating = 0;
 }
 
+/** Called by tap_handler(). */
 static void animate_date() {
-  if (dateIsAnimating) return;
-  dateIsAnimating = 1;
- 
-  PropertyAnimation *in = property_animation_create_layer_frame(
-    (Layer*) s_date_layer, &date_frame_offscreen, &date_frame_onscreen);
-  animation_set_duration((Animation*) in, DATE_ANIMATION_DURATION_IN);
-  animation_set_curve((Animation*) in, AnimationCurveEaseInOut);
-  animation_schedule((Animation*) in);
-
-  PropertyAnimation *out = property_animation_create_layer_frame(
-    (Layer*) s_date_layer, &date_frame_onscreen, &date_frame_offscreen);
-  animation_set_duration((Animation*) out, DATE_ANIMATION_DURATION_OUT);
-  animation_set_delay((Animation*) out, DATE_VISIBILITY_DURATION);
-  animation_set_handlers((Animation*) out, (AnimationHandlers) {
-    .stopped = date_animation_stopped_handler
-  }, NULL);
-  animation_set_curve((Animation*) out, AnimationCurveEaseIn);
-  animation_schedule((Animation*) out);
+    if (dateIsAnimating) return;
+    dateIsAnimating = 1;
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Running animation");
+    
+    PropertyAnimation *in = property_animation_create_layer_frame(
+        (Layer*) s_date_layer, &date_frame_offscreen, &date_frame_onscreen);
+    animation_set_duration((Animation*) in, DATE_ANIMATION_DURATION_IN);
+    animation_set_curve((Animation*) in, AnimationCurveEaseInOut);
+    animation_schedule((Animation*) in);
+    
+    PropertyAnimation *out = property_animation_create_layer_frame(
+        (Layer*) s_date_layer, &date_frame_onscreen, &date_frame_offscreen);
+    animation_set_duration((Animation*) out, DATE_ANIMATION_DURATION_OUT);
+    animation_set_delay((Animation*) out, DATE_VISIBILITY_DURATION);
+    animation_set_handlers((Animation*) out, (AnimationHandlers) {
+        .stopped = date_animation_stopped_handler
+    }, NULL);
+    animation_set_curve((Animation*) out, AnimationCurveEaseIn);
+    animation_schedule((Animation*) out);
 }
 
+
+/** Called by tick_handler(). */
 static void draw_clock(struct tm *tick_time) {
-  const int64_t mins_in_day = 24 * 60;
-  const int64_t mins_since_midnight = tick_time->tm_hour * 60 + tick_time->tm_min;
-  const int64_t background_x_offset = mins_since_midnight * BACKGROUND_WIDTH * 2 / mins_in_day;
-
-  for (int i = 0; i < 4; i++) {
-    GRect frame = GRect((-background_x_offset) + (SCREEN_WIDTH / 2) + BACKGROUND_WIDTH * (i - 1), 0, BACKGROUND_WIDTH, SCREEN_HEIGHT);
-    layer_set_frame(bitmap_layer_get_layer(s_background_layers[i]), frame);
-  }
+    const int64_t mins_in_day = 24 * 60;
+    const int64_t mins_since_midnight = tick_time->tm_hour * 60 + tick_time->tm_min;
+    const int64_t background_x_offset = mins_since_midnight * BACKGROUND_WIDTH * 2 / mins_in_day;
+    
+    for (int i = 0; i < 4; i++) {
+        GRect frame = GRect((-background_x_offset) + (SCREEN_WIDTH / 2) + BACKGROUND_WIDTH * (i - 1), 0, BACKGROUND_WIDTH, SCREEN_HEIGHT);
+        layer_set_frame(bitmap_layer_get_layer(s_background_layers[i]), frame);
+    }
 }
 
+/** Called by tick_handler(). */
 static void draw_date(struct tm *tick_time) {
-  static char buffer[7];
-  snprintf(buffer, sizeof(buffer), "%s %d", days_of_week[tick_time->tm_wday], tick_time->tm_mday);
-  //APP_LOG(APP_LOG_LEVEL_DEBUG, buffer);
-  text_layer_set_text(s_date_layer, buffer);
+    static char buffer[7];
+    snprintf(buffer, sizeof(buffer), "%s %d", days_of_week[tick_time->tm_wday], tick_time->tm_mday);
+    //APP_LOG(APP_LOG_LEVEL_DEBUG, buffer);
+    text_layer_set_text(s_date_layer, buffer);
 }
 
+
+/**
+ * Assigned in init().
+ * Calls animate_date().
+ */
 static void tap_handler(AccelAxisType axis, int32_t direction) {
-  animate_date();
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "In tap_hander()");
+    animate_date();
 }
 
+
+/**
+ * Assigned in init().
+ * Calls draw_clock() and draw_date().
+ */
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  draw_clock(tick_time);
-  draw_date(tick_time);
+    draw_clock(tick_time);
+    draw_date(tick_time);
 }
 
+
+/** Assigned in main_window_load(). */
 static void needle_layer_update_proc(Layer *layer, GContext *ctx) {
-  graphics_context_set_fill_color(ctx, GColorOrange);
-  graphics_fill_rect(ctx, GRect(SCREEN_WIDTH / 2 - 1, 0, 2, SCREEN_HEIGHT * 0.6), 0, 0);
+    graphics_context_set_fill_color(ctx, GColorOrange);
+    graphics_fill_rect(ctx, GRect(SCREEN_WIDTH / 2 - 1, 0, 2, SCREEN_HEIGHT * 0.6), 0, 0);
 }
+
 
 static void main_window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
-
-  Layer *needle_layer = layer_create(bounds);
-  layer_set_update_proc(needle_layer, needle_layer_update_proc);
-
-  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
-  for (unsigned i = 0; i < ARRAY_LENGTH(s_background_layers); i++) {
-    s_background_layers[i] = bitmap_layer_create(bounds);
-    bitmap_layer_set_bitmap(s_background_layers[i], s_background_bitmap);
-    layer_add_child(window_layer, (Layer*) s_background_layers[i]);
-  }
-
-  date_frame_onscreen = GRect(SCREEN_WIDTH / 2 + 2, PBL_IF_ROUND_ELSE(30, 20), PBL_IF_ROUND_ELSE(67, SCREEN_WIDTH / 2 - 2), 15);
-  date_frame_offscreen = (GRect) { .origin = GPoint(date_frame_onscreen.origin.x, -50), .size = date_frame_onscreen.size };
-  
-  GFont date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_MEDIUM_14));
-  s_date_layer = text_layer_create(date_frame_offscreen);
-  text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
-  text_layer_set_text_color(s_date_layer, GColorWhite);
-  text_layer_set_font(s_date_layer, date_font);
-  text_layer_set_background_color(s_date_layer, GColorBlack);
-
-  s_battery_meter_layer = layer_create(bounds);
-  layer_set_update_proc(s_battery_meter_layer, battery_meter_update_proc);
-  
-  s_battery_percent_layer = text_layer_create(GRect(10, 10, 60, 60));
-  text_layer_set_text_color(s_battery_percent_layer, GColorWhite);
-  text_layer_set_background_color(s_battery_percent_layer, GColorClear);
-  text_layer_set_text_alignment(s_battery_percent_layer, GTextAlignmentLeft);
-  text_layer_set_font(s_date_layer, date_font);
-  layer_set_update_proc(text_layer_get_layer(s_battery_percent_layer), battery_percent_update_proc);
-  
-  layer_add_child(window_layer, needle_layer);
-  layer_add_child(window_layer, (Layer*) s_date_layer);
-  layer_add_child(window_layer, s_battery_meter_layer);
-  layer_add_child(window_layer, text_layer_get_layer(s_battery_percent_layer));
+    Layer *window_layer = window_get_root_layer(window);
+    GRect bounds = layer_get_bounds(window_layer);
+    
+    
+    // BACKGROUND
+    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
+    for (unsigned i = 0; i < ARRAY_LENGTH(s_background_layers); i++) {
+        s_background_layers[i] = bitmap_layer_create(bounds);
+        bitmap_layer_set_bitmap(s_background_layers[i], s_background_bitmap);
+        layer_add_child(window_layer, (Layer*) s_background_layers[i]);
+    }
+    
+    
+    // NEDLE
+    Layer *needle_layer = layer_create(bounds);
+    layer_set_update_proc(needle_layer, needle_layer_update_proc);
+    layer_add_child(window_layer, needle_layer);
+    
+    
+    // DATE
+    date_frame_onscreen = GRect(SCREEN_WIDTH / 2 + 2, PBL_IF_ROUND_ELSE(30, 20), PBL_IF_ROUND_ELSE(67, SCREEN_WIDTH / 2 - 2), 15);
+    date_frame_offscreen = (GRect) { .origin = GPoint(date_frame_onscreen.origin.x, -50), .size = date_frame_onscreen.size };
+    GFont date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_MEDIUM_14));
+    s_date_layer = text_layer_create(date_frame_offscreen);
+    text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+    text_layer_set_text_color(s_date_layer, GColorWhite);
+    text_layer_set_font(s_date_layer, date_font);
+    text_layer_set_background_color(s_date_layer, GColorBlack);
+    layer_add_child(window_layer, (Layer*) s_date_layer);
+    
+    
+    // BATTERY METER
+    s_battery_meter_layer = layer_create(bounds);
+    layer_set_update_proc(s_battery_meter_layer, battery_meter_update_proc);
+    layer_add_child(window_layer, s_battery_meter_layer);
+    
+    
+    // BATTERY PERCENT
+    s_battery_percent_layer = text_layer_create(GRect(0, 0, 144, 154));
+    text_layer_set_text(s_battery_percent_layer, "000%");
+    text_layer_set_font(s_battery_percent_layer, date_font);
+    text_layer_set_text_alignment(s_battery_percent_layer, GTextAlignmentLeft);
+    text_layer_set_background_color(s_battery_percent_layer, GColorClear);
+    text_layer_set_text_color(s_battery_percent_layer, GColorWhite);
+    layer_add_child(window_layer, text_layer_get_layer(s_battery_percent_layer));
 
 }
 
 static void main_window_unload(Window *window) {
-  for (unsigned i = 0; i < ARRAY_LENGTH(s_background_layers); i++) {
-    bitmap_layer_destroy(s_background_layers[i]);
-  }
-  gbitmap_destroy(s_background_bitmap);
-  text_layer_destroy(s_date_layer);
-  layer_destroy(s_battery_meter_layer);
-  text_layer_destroy(s_battery_percent_layer);
+    for (unsigned i = 0; i < ARRAY_LENGTH(s_background_layers); i++) {
+        bitmap_layer_destroy(s_background_layers[i]);
+    }
+    gbitmap_destroy(s_background_bitmap);
+    text_layer_destroy(s_date_layer);
+    text_layer_destroy(s_battery_percent_layer);
+    layer_destroy(s_battery_meter_layer);
 
 }
 
 static void init() {
-  s_main_window = window_create();
-  window_set_background_color(s_main_window, GColorBlack);
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
-  window_stack_push(s_main_window, true);
-
-  time_t current_time = time(NULL);
-  draw_clock(localtime(&current_time));
-  draw_date(localtime(&current_time));
-
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-  accel_tap_service_subscribe(tap_handler);
-  
-  battery_state_service_subscribe(battery_callback);
-  
-  // Ensure battery level is displayed from the start
-  battery_callback(battery_state_service_peek());
+    s_main_window = window_create();
+    window_set_background_color(s_main_window, GColorBlack);
+    window_set_window_handlers(s_main_window, (WindowHandlers) {
+        .load = main_window_load,
+        .unload = main_window_unload
+    });
+    window_stack_push(s_main_window, true);
+    
+    time_t current_time = time(NULL);
+    draw_clock(localtime(&current_time));
+    draw_date(localtime(&current_time));
+    
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+    accel_tap_service_subscribe(tap_handler);
+    battery_state_service_subscribe(battery_handler);
+     APP_LOG(APP_LOG_LEVEL_DEBUG, "Thing");
+    
+    // Ensure battery level is displayed from the start
+    battery_handler(battery_state_service_peek());
 
 }
 
 static void deinit() {
-  window_destroy(s_main_window);
+    window_destroy(s_main_window);
 }
 
 int main(void) {
-  init();
-  app_event_loop();
-  deinit();
+    init();
+    app_event_loop();
+    deinit();
 }

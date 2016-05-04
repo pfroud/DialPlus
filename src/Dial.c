96160cdc4beff1706bb86b8c6dd0cf24cb7d4606
Peter Froud <pfroud+github@gmail.com>
2016-05-03 23:52:59 -0700

    Better organize code

--- src/Dial.c
+++ src/Dial.c
@@ -4,11 +4,9 @@
 #define SCREEN_WIDTH (PBL_IF_ROUND_ELSE(180, 144))
 #define SCREEN_HEIGHT (PBL_IF_ROUND_ELSE(180, 168))
 
-#define DATE_ANIMATION_DURATION_IN (400)
-#define DATE_ANIMATION_DURATION_OUT (200)
-#define DATE_VISIBILITY_DURATION (4000)
 
-# define BAR_THICKNESS (2)
+
+#define BAR_THICKNESS (2)
 
 
 static Window *s_main_window;
@@ -17,137 +15,12 @@ static GBitmap *s_background_bitmap;
 static TextLayer *s_date_layer, *s_batt_percent_layer;
 static Layer *needle_layer, *s_batt_bar_layer, *s_event_mark_layer;
 
-static GRect date_frame_onscreen, date_frame_offscreen;
-static GRect batt_bar_frame_onscreen, batt_bar_frame_offscreen;
-static GRect batt_percent_frame_onscreen, batt_percent_frame_offscreen;
+static int last_mins_since_midnight, s_batt_level;
 
-bool isAnimating = 0;
-static int last_mins_since_midnight;
-
-static const char *days_of_week[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
 static const int NEEDLE_X_START = SCREEN_WIDTH / 2 - 1;
 
-static int s_batt_level;
 
-static void batt_handler(BatteryChargeState state) {
-    s_batt_level = state.charge_percent;
-    
-    static char buffer[5];
-    snprintf(buffer, sizeof(buffer), "%d%%", s_batt_level);
-    text_layer_set_text(s_batt_percent_layer, buffer);
-    
-    layer_mark_dirty(s_batt_bar_layer);
-    layer_mark_dirty(text_layer_get_layer(s_batt_percent_layer));
-}
-
-static void batt_bar_update_proc(Layer *layer, GContext *ctx) {
-    int batt = s_batt_level;
-    GColor theColor = GColorRed;
-    if(batt >= 20) theColor = GColorYellow;
-    if(batt >= 40) theColor = GColorGreen;
-    graphics_context_set_fill_color(ctx, theColor);
-    
-    int barWidth = batt * SCREEN_WIDTH / 100; //can't use floating point numbers!
-    GRect theBar = GRect(0, 0, barWidth, BAR_THICKNESS); // x, y, w, h
-    graphics_fill_rect(ctx, theBar, 0, GCornerNone);
-}
-
-
-static void animation_stopped_handler(Animation *animation, bool finished, void *context) {
-    isAnimating = 0;
-}
-
-static void animate_batt_bar(){
-    PropertyAnimation *in = property_animation_create_layer_frame(
-        (Layer*) s_batt_bar_layer, &batt_bar_frame_offscreen, &batt_bar_frame_onscreen);
-    animation_set_duration((Animation*) in, DATE_ANIMATION_DURATION_IN);
-    animation_set_curve((Animation*) in, AnimationCurveEaseInOut);
-    animation_schedule((Animation*) in);
-    
-    PropertyAnimation *out = property_animation_create_layer_frame(
-        (Layer*) s_batt_bar_layer, &batt_bar_frame_onscreen, &batt_bar_frame_offscreen);
-    animation_set_duration((Animation*) out, DATE_ANIMATION_DURATION_OUT);
-    animation_set_delay((Animation*) out, DATE_VISIBILITY_DURATION);
-    animation_set_curve((Animation*) out, AnimationCurveEaseIn);
-    animation_schedule((Animation*) out);
-}
-
-static void animate_batt_percent(){
-    PropertyAnimation *in = property_animation_create_layer_frame(
-        (Layer*) s_batt_percent_layer, &batt_percent_frame_offscreen, &batt_percent_frame_onscreen);
-    animation_set_duration((Animation*) in, DATE_ANIMATION_DURATION_IN);
-    animation_set_curve((Animation*) in, AnimationCurveEaseInOut);
-    animation_schedule((Animation*) in);
-    
-    PropertyAnimation *out = property_animation_create_layer_frame(
-        (Layer*) s_batt_percent_layer, &batt_percent_frame_onscreen, &batt_percent_frame_offscreen);
-    animation_set_duration((Animation*) out, DATE_ANIMATION_DURATION_OUT);
-    animation_set_delay((Animation*) out, DATE_VISIBILITY_DURATION);
-    animation_set_curve((Animation*) out, AnimationCurveEaseIn);
-    animation_schedule((Animation*) out);
-}
-
-/**
- * Assigned in init().
- * Calls animate_date().
- */
-static void tap_handler(AccelAxisType axis, int32_t direction) {
-    if (isAnimating) return;
-    isAnimating = 1;
-    
-    animate_batt_bar();
-    animate_batt_percent();
-    
-    PropertyAnimation *in = property_animation_create_layer_frame(
-        (Layer*) s_date_layer, &date_frame_offscreen, &date_frame_onscreen);
-    animation_set_duration((Animation*) in, DATE_ANIMATION_DURATION_IN);
-    animation_set_curve((Animation*) in, AnimationCurveEaseInOut);
-    animation_schedule((Animation*) in);
-    
-    PropertyAnimation *out = property_animation_create_layer_frame(
-        (Layer*) s_date_layer, &date_frame_onscreen, &date_frame_offscreen);
-    animation_set_duration((Animation*) out, DATE_ANIMATION_DURATION_OUT);
-    animation_set_delay((Animation*) out, DATE_VISIBILITY_DURATION);
-    animation_set_handlers((Animation*) out, (AnimationHandlers) {
-        .stopped = animation_stopped_handler
-    }, NULL);
-    animation_set_curve((Animation*) out, AnimationCurveEaseIn);
-    animation_schedule((Animation*) out);
-}
-
-
-/**
- * Assigned in init().
- * Calls draw_clock() and draw_date().
- */
-static void tick_handler_minute(struct tm *tick_time, TimeUnits units_changed) {
-    const int mins_since_midnight = tick_time->tm_hour * 60 + tick_time->tm_min;
-    const int background_x_offset = mins_since_midnight * BACKGROUND_WIDTH * 2 / MINUTES_PER_DAY;
-    last_mins_since_midnight = mins_since_midnight;
-    
-    for (int i = 0; i < 4; i++) {
-        GRect frame = GRect((-background_x_offset) + (SCREEN_WIDTH / 2) + BACKGROUND_WIDTH * (i - 1), 0, BACKGROUND_WIDTH, SCREEN_HEIGHT);
-        layer_set_frame(bitmap_layer_get_layer(s_background_layers[i]), frame);
-    }
-}
-
-/**
- * Assigned in init().
- * Calls draw_clock() and draw_date().
- */
-static void tick_handler_day(struct tm *tick_time, TimeUnits units_changed) {
-    static char buffer[7];
-    snprintf(buffer, sizeof(buffer), "%s %d", days_of_week[tick_time->tm_wday], tick_time->tm_mday);
-    text_layer_set_text(s_date_layer, buffer);
-}
-
-
-/** Assigned in main_window_load(). */
-static void needle_layer_update_proc(Layer *layer, GContext *ctx) {
-    graphics_context_set_fill_color(ctx, GColorOrange);
-    graphics_fill_rect(ctx, GRect(NEEDLE_X_START, 0, 2, SCREEN_HEIGHT * 0.6), 0, 0);
-}
-
+//// in progress things ////
 
 static void draw_event_mark(GContext *ctx, int event_time_start, int event_time_end){
     
@@ -174,11 +47,144 @@ static void draw_event_mark(GContext *ctx, int event_time_start, int event_time_
     graphics_fill_rect(ctx, GRect(x_start, -MARK_HEIGHT-1, MARK_THICKNESS, MARK_HEIGHT), 0, GCornerNone);
     graphics_fill_rect(ctx, GRect(x_end,   -MARK_HEIGHT-1, MARK_THICKNESS, MARK_HEIGHT), 0, GCornerNone);
     graphics_fill_rect(ctx, GRect(x_start, -MARK_HEIGHT-1, x_end-x_start, MARK_THICKNESS), 0, GCornerNone);
+    
+    // https://developer.pebble.com/docs/c/Graphics/Drawing_Paths/
+    const GPathInfo TRIANGLE_PATH_INFO = {
+        .num_points = 3,
+        .points = (GPoint []) {{21, 0}, {14, 26}, {28, 26}}
+    };
+    GPath *s_my_path_ptr = gpath_create(&TRIANGLE_PATH_INFO);
+    gpath_draw_filled(ctx, s_my_path_ptr);
+    
+}
+
+
+//// ANIMATION ////
+
+#define ANIM_DURATION_IN (400)
+#define ANIM_DURATION_OUT (200)
+#define ANIM_VISIBILITY_DURATION (4000)
+static GRect date_frame_onscreen, date_frame_offscreen;
+static GRect batt_bar_frame_onscreen, batt_bar_frame_offscreen;
+static GRect batt_percent_frame_onscreen, batt_percent_frame_offscreen;
+bool isAnimating = 0;
+
+static void animate_batt_bar(){
+    PropertyAnimation *in = property_animation_create_layer_frame(
+        (Layer*) s_batt_bar_layer, &batt_bar_frame_offscreen, &batt_bar_frame_onscreen);
+    animation_set_duration((Animation*) in, ANIM_DURATION_IN);
+    animation_set_curve((Animation*) in, AnimationCurveEaseInOut);
+    animation_schedule((Animation*) in);
+    
+    PropertyAnimation *out = property_animation_create_layer_frame(
+        (Layer*) s_batt_bar_layer, &batt_bar_frame_onscreen, &batt_bar_frame_offscreen);
+    animation_set_duration((Animation*) out, ANIM_DURATION_OUT);
+    animation_set_delay((Animation*) out, ANIM_VISIBILITY_DURATION);
+    animation_set_curve((Animation*) out, AnimationCurveEaseIn);
+    animation_schedule((Animation*) out);
+}
+
+static void animate_batt_percent(){
+    PropertyAnimation *in = property_animation_create_layer_frame(
+        (Layer*) s_batt_percent_layer, &batt_percent_frame_offscreen, &batt_percent_frame_onscreen);
+    animation_set_duration((Animation*) in, ANIM_DURATION_IN);
+    animation_set_curve((Animation*) in, AnimationCurveEaseInOut);
+    animation_schedule((Animation*) in);
+    
+    PropertyAnimation *out = property_animation_create_layer_frame(
+        (Layer*) s_batt_percent_layer, &batt_percent_frame_onscreen, &batt_percent_frame_offscreen);
+    animation_set_duration((Animation*) out, ANIM_DURATION_OUT);
+    animation_set_delay((Animation*) out, ANIM_VISIBILITY_DURATION);
+    animation_set_curve((Animation*) out, AnimationCurveEaseIn);
+    animation_schedule((Animation*) out);
+}
+
+static void animation_stopped_handler(Animation *animation, bool finished, void *context) {
+    isAnimating = 0;
+}
+
+
+//// HANDLERS ////
+
+static void tap_handler(AccelAxisType axis, int32_t direction) {
+    if (isAnimating) return;
+    isAnimating = 1;
+    
+    animate_batt_bar();
+    animate_batt_percent();
+    
+    PropertyAnimation *in = property_animation_create_layer_frame(
+        (Layer*) s_date_layer, &date_frame_offscreen, &date_frame_onscreen);
+    animation_set_duration((Animation*) in, DATE_ANIMATION_DURATION_IN);
+    animation_set_curve((Animation*) in, AnimationCurveEaseInOut);
+    animation_schedule((Animation*) in);
+    
+    PropertyAnimation *out = property_animation_create_layer_frame(
+        (Layer*) s_date_layer, &date_frame_onscreen, &date_frame_offscreen);
+    animation_set_duration((Animation*) out, DATE_ANIMATION_DURATION_OUT);
+    animation_set_delay((Animation*) out, DATE_VISIBILITY_DURATION);
+    animation_set_handlers((Animation*) out, (AnimationHandlers) {
+        .stopped = animation_stopped_handler
+    }, NULL);
+    animation_set_curve((Animation*) out, AnimationCurveEaseIn);
+    animation_schedule((Animation*) out);
+}
+
+static void batt_handler(BatteryChargeState state) {
+    s_batt_level = state.charge_percent;
+    
+    static char buffer[5];
+    snprintf(buffer, sizeof(buffer), "%d%%", s_batt_level);
+    text_layer_set_text(s_batt_percent_layer, buffer);
+    
+    layer_mark_dirty(s_batt_bar_layer);
+    layer_mark_dirty(text_layer_get_layer(s_batt_percent_layer));
+}
+
+static void tick_handler_minute(struct tm *tick_time, TimeUnits units_changed) {
+    const int mins_since_midnight = tick_time->tm_hour * 60 + tick_time->tm_min;
+    const int background_x_offset = mins_since_midnight * BACKGROUND_WIDTH * 2 / MINUTES_PER_DAY;
+    last_mins_since_midnight = mins_since_midnight;
+    
+    for (int i = 0; i < 4; i++) {
+        GRect frame = GRect((-background_x_offset) + (SCREEN_WIDTH / 2) + BACKGROUND_WIDTH * (i - 1), 0, BACKGROUND_WIDTH, SCREEN_HEIGHT);
+        layer_set_frame(bitmap_layer_get_layer(s_background_layers[i]), frame);
+    }
+}
+
+static const char *days_of_week[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+static void tick_handler_day(struct tm *tick_time, TimeUnits units_changed) {
+    static char buffer[7];
+    snprintf(buffer, sizeof(buffer), "%s %d", days_of_week[tick_time->tm_wday], tick_time->tm_mday);
+    text_layer_set_text(s_date_layer, buffer);
+}
+
+
+//// UPDATE PROCS ////
+
+static void needle_layer_update_proc(Layer *layer, GContext *ctx) {
+    graphics_context_set_fill_color(ctx, GColorOrange);
+    graphics_fill_rect(ctx, GRect(NEEDLE_X_START, 0, 2, SCREEN_HEIGHT * 0.6), 0, 0);
 }
 
 static void event_mark_update_proc(Layer *layer, GContext *ctx) {
     draw_event_mark(ctx, 60*(12+4) + 10, 60*(12+4) + 30);
 }
+
+static void batt_bar_update_proc(Layer *layer, GContext *ctx) {
+    int batt = s_batt_level;
+    GColor theColor = GColorRed;
+    if(batt >= 20) theColor = GColorYellow;
+    if(batt >= 40) theColor = GColorGreen;
+    graphics_context_set_fill_color(ctx, theColor);
+    
+    int barWidth = batt * SCREEN_WIDTH / 100; //can't use floating point numbers!
+    GRect theBar = GRect(0, 0, barWidth, BAR_THICKNESS); // x, y, w, h
+    graphics_fill_rect(ctx, theBar, 0, GCornerNone);
+}
+
+
+//// CORE ////
 
 static void main_window_load(Window *window) {
     Layer *window_layer = window_get_root_layer(window);
